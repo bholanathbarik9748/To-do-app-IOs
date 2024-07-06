26b7487a2bddcaeaa8f8951a9cc3895743785668
Bholanath <bholanathbarik9748@gmail.com>
2024-07-06 19:59:14 +0530

    Feat : add complete task and delete functionality

--- Tudo List Ios/ContentView.swift
+++ Tudo List Ios/ContentView.swift
@@ -8,14 +8,56 @@
 import SwiftUI
 
 struct ContentView: View {
+    @StateObject private var viewModel = TudoViewModel()
+    @State private var tudoValue = "";
+    
     var body: some View {
-        VStack {
-            Image(systemName: "globe")
-                .imageScale(.large)
-                .foregroundStyle(.tint)
-            Text("Hello, world!")
+        NavigationView{
+            VStack{
+                TextField("Pls enter your to-do",text: $tudoValue)
+                    .textFieldStyle(RoundedBorderTextFieldStyle())
+                    .padding()
+                
+                Button(action: {
+                    viewModel.add(title: tudoValue)
+                    tudoValue = "";
+                }, label: {
+                    Text("Submit")
+                        .padding()
+                        .background(Color.blue)
+                        .foregroundColor(.white)
+                        .cornerRadius(10)
+                })
+                .padding()
+                
+                List{
+                    ForEach(viewModel.tudos) { tudo in
+                        HStack {
+                            Text(tudo.title)
+                                .strikethrough(tudo.isCompleted , color: .green)
+                                .foregroundStyle(tudo.isCompleted ? Color.green : Color.black)
+                            
+                            Spacer()
+                            
+                            if !tudo.isCompleted {
+                                Button(action: {
+                                    viewModel.toggleComplete(for: tudo)
+                                }, label: {
+                                    Image(systemName: "checkmark.circle")
+                                        .foregroundColor(.green)
+                                })
+                            }
+                        }
+                    }
+                    .onDelete(perform: { indexSet in
+                        viewModel.deleteTudo(at: indexSet)
+                    })
+                }
+            }
+            .navigationBarTitle("To-Do List")
+            .padding()
         }
-        .padding()
+        
     }
 }
 
--- Tudo List Ios/ViewModel/TudoViewModel.swift
+++ Tudo List Ios/ViewModel/TudoViewModel.swift
@@ -1,47 +1,56 @@
 //
 //  TudoViewModel.swift
-//  Tudo List Ios
+//  Tudo List iOS
 //
 //  Created by Bholanath Barik on 03/07/24.
 //
 
 import Foundation
 
-class TudoViewModel : ObservableObject {
-    @Published var tudos: [TodoModel] = []
-    private let objectkey = "TudoList";
+// ViewModel for managing a list of to-dos
+class TudoViewModel: ObservableObject {
+    @Published var tudos: [TodoModel] = []  // Published array to hold todo items
+    private let objectKey = "TudoList"  // Key for saving/loading data from UserDefaults
     
+    // Initializer to load saved todos when the view model is created
     init() {
-        loadTudo();
+        loadTudo()
     }
     
-    private func saveTudo(){
+    // Function to save the current list of todos to UserDefaults
+    private func saveTudo() {
         if let encodedData = try? JSONEncoder().encode(tudos) {
-            UserDefaults.standard.setValue(encodedData, forKey: objectkey)
+            UserDefaults.standard.setValue(encodedData, forKey: objectKey)
         }
     }
     
-    private func loadTudo(){
-        if let savedData = UserDefaults.standard.data(forKey: objectkey),
+    // Function to load the list of todos from UserDefaults
+    private func loadTudo() {
+        if let savedData = UserDefaults.standard.data(forKey: objectKey),
            let savedTudos = try? JSONDecoder().decode([TodoModel].self, from: savedData) {
             tudos = savedTudos
         }
     }
     
+    // Function to add a new todo item to the list
     func add(title: String) {
-        let newTudo = TodoModel(id: UUID(), title: title, isCompleted: false);
-        tudos.append(newTudo);
-        saveTudo();
+        let newTudo = TodoModel(id: UUID(), title: title, isCompleted: false)
+        tudos.append(newTudo)
+        saveTudo()
+        loadTudo()
     }
     
-    func deleteTudo(at offsets: IndexSet){
-        tudos.remove(atOffsets: offsets);
-        saveTudo();
+    // Function to delete todo items from the list at specified offsets
+    func deleteTudo(at offsets: IndexSet) {
+        tudos.remove(atOffsets: offsets)
+        saveTudo()
     }
     
-    func toggleComplete(for todo : TodoModel) {
+    // Function to mark a todo item as completed
+    func toggleComplete(for todo: TodoModel) {
         if let index = tudos.firstIndex(where: { $0.id == todo.id }) {
-            tudos[index].isCompleted = true;
+            tudos[index].isCompleted = true
         }
+        saveTudo()
     }
 }
